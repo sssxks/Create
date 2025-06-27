bb68b4d9393ae1367e3432a7d7e6216e09d0a176
ks <2777389616@qq.com>
2025-06-27 20:33:38 +0800

    added color to display linked sign

--- src/main/java/com/simibubi/create/content/redstone/displayLink/target/SignDisplayTarget.java
+++ src/main/java/com/simibubi/create/content/redstone/displayLink/target/SignDisplayTarget.java
@@ -29,8 +29,8 @@ public class SignDisplayTarget extends DisplayTarget {
 				break;
 
 			final int iFinal = i;
-			String content = text.get(iFinal).getString(sign.getMaxTextLineWidth());
-			signText = signText.map(st -> st.setMessage(iFinal + line, Component.literal(content)));
+			MutableComponent content = text.get(iFinal);
+			signText = signText.map(st -> st.setMessage(iFinal + line, content));
 			changed = true;
 		}
 
